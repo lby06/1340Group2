8882fbef13703ba5148d7fe3ea5b1922f8147f44
Ayiinnn <158124586+Ayiinnn@users.noreply.github.com>
2024-03-28 15:39:08 +0800

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -47,7 +47,7 @@ class main_character
 
   private:
      int hp,mp,exp,attack;
-     doube critical_rate,critical_damage,evasion_rate,mp_recover_rate.
+     double critical_rate,critical_damage,evasion_rate,mp_recover_rate.
      int skill_status[6],equipment_status[6];
 };
 int main()
