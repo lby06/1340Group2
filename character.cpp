e5cc7f66743d4c74e7f095d1a43898cf0d15cfab
Ayiinnn <158124586+Ayiinnn@users.noreply.github.com>
2024-04-01 17:43:44 +0800

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -32,7 +32,7 @@ class main_character
      void display()
      {
        cout<<"LEVEL : "<<level<<endl;
-       cout<<"HP: "<<hp<<"\n"<<"attack: "<<atk<<"\n"<<"defence: "<<def<<"\n"<<"MP: "<<mp<<"\n"<<"EXP: "<<exp<<"\n";
+       cout<<"HP: "<<hp<<"("<<hp_max<<")"<<"\n"<<"attack: "<<atk<<"\n"<<"defence: "<<def<<"\n"<<"MP: "<<mp<<"\n"<<"EXP: "<<exp<<"\n";
        cout<<"Critical Rate: "<<critical_rate*100<<"%"<<"\n"<<"Critical damage: "<<critical_damage*100<<"%"<<"\n";
        cout<<"Evasion Rate: "<<evasion_rate*100<<"%"<<"\n";
      }
@@ -58,7 +58,7 @@ class main_character
       def+=level/3;
       hp_max=15*level+10;
       hp=hp_max;
-      mp=2*level;
+      mp=2*level+3;
       critical_rate+=level*0.006;
       critical_damage+=0.1;
       evasion_rate+=0.02;
@@ -67,6 +67,70 @@ class main_character
         critical_rate=1;
       }
     }
+    void hp_recover(int x)
+    {
+        if ((hp_max-hp)>=x)
+        {
+          hp+=x;
+        }
+        else 
+        {
+          hp=hp_max;
+        }
+    }
+    void mp_recover(int x)
+    {
+      mp+=x;
+    }
+    void hurt(int x)
+    {
+      random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<> dis(0, 1);
+        if (dis(gen)<=evasion_rate) 
+        {
+           cout<<"Evasion successful.";
+        }
+        else
+        {
+           int y=x-def;
+           if ((hp-y)<=0)
+           {
+             hp=0;
+           }
+           else 
+           {
+             hp-=y;
+           }
+        }
+    }
+    void activate_recoverhit()
+    {
+      skill_status[0]=1;
+    }
+    int act_recoverhit()
+    {
+      if (skill_status[0]==1)
+      {
+        if (mp>=4)
+        {
+          mp-=4;
+          hp_recover(atk*1.5);
+          return atk*1.5;
+        }
+        else
+        {
+          cout<<"Magic power is deficient.";
+          return 0;
+        }      
+      }
+      else
+      {
+        cout<<"<recover hit> is not activated yet.";
+        return 0;
+      }
+    }
+    
 
   private:
      int locate_x,locate_y;
@@ -79,10 +143,20 @@ int main()
   main_character cha1;
   cha1.reset();
   cha1.display();
-  for (int i=0;i<=10;i++)
+  for (int i=0;i<=9;i++)
   {
       cha1.upgrade();
       cout<<endl;
       cha1.display();
-      }
   }
+  cha1.hurt(20);
+  cha1.display();
+  cout<<endl;
+  cha1.activate_recoverhit();
+  cout<<cha1.act_recoverhit();
+  cout<<endl;
+  cha1.display();
+  cout<<endl;
+
+}
+   
