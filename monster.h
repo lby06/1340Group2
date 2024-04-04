0b4839d6d5ee2d9cf66680f20a1873390855ba60
Ayiinnn <158124586+Ayiinnn@users.noreply.github.com>
2024-04-04 14:23:42 +0800

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,78 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+#include <monster.h>
+#include <random>
+#include <iostream>
+
+class Clone {
+public:
+    bool is_alive();
+    void reset(int level, int number);
+    int normalattack();
+    void hp_recover(int x);
+    void mp_recover(int x);
+    void hurt(int x);
+
+private:
+    int hp, hp_max, atk, def, mp;
+    double critical_rate, critical_damage;
+    double evasion_rate;
+    int damage();
+};
+
+class Robot {
+public:
+    bool is_alive();
+    void reset(int level, int number);
+    int normalattack();
+    void hp_recover(int x);
+    void mp_recover(int x);
+    void hurt(int x);
+    int lasers();
+
+private:
+    int hp, hp_max, atk, def, mp;
+    double critical_rate, critical_damage;
+    double evasion_rate;
+    int damage();
+};
+
+class Cith {
+public:
+    bool is_alive();
+    void reset(int level, int number);
+    int normalattack();
+    void hp_recover(int x);
+    void mp_recover(int x);
+    void returntodark();
+    void hurt(int x);
+    int lasers();
+
+private:
+    int hp, hp_max, atk, def, mp;
+    double critical_rate, critical_damage;
+    double evasion_rate;
+    int life;
+    int damage();
+};
+
+class Mandalorians {
+public:
+    bool is_alive();
+    void reset(int level, int number);
+    int normalattack();
+    void hp_recover(int x);
+    void mp_recover(int x);
+    void rage();
+    void hurt(int x);
+
+private:
+    int hp, hp_max, atk, def, mp;
+    double critical_rate, critical_damage;
+    double evasion_rate;
+    int beatback;
+    int damreturn;
+    int damage();
+};
+
+#endif
