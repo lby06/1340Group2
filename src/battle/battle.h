29fadf043e90f7c650ceaba2bda8ba859f015201
lby06 <147181260+lby06@users.noreply.github.com>
2024-04-09 21:08:21 +0800

    Create battle.h

---
+++ src/battle/battle.h
@@ -0,0 +1,13 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+
+#include "../character/character.h"
+#include "../character/monster.h"
+#include "../character/boss.h"
+#include "../utils/utils.hpp"
+#include "../battle/print_features.h"
+
+
+void battle_monster(std::string name_mc,std::string name_ms);
+
+#endif // BATTLE_H
