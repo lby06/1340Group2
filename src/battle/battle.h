712027211aecf8b716bd4b8a613b94145d243614
lby06 <147181260+lby06@users.noreply.github.com>
2024-04-21 17:06:07 +0800

    Update battle.h

--- src/battle/battle.h
+++ src/battle/battle.h
@@ -1,13 +1,14 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-#include "../character/character.h"
-#include "../character/monster.h"
-#include "../battle/print_features.h"
+#include "character.h"
+#include "monster.h"
+#include "print_features.h"
+#include <string>
 
 //call battle_monster when encountering monster.
 //name_mc for ASCii of different level, name_ms for type of monster to fight with
-int readKeyboard()
-void battle_monster(std::int level,std::string name_ms);
+int readKeyboard();
+void battle_monster(int level,std::string name_ms);
 
 #endif // BATTLE_H
